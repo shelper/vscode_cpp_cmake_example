cf1da9cd39e21e50e18d1a979f6b2a58632f7337
zhijia <zhijia.yuan@zebra.com>
2022-12-07 15:48:53 -0500

    remove gtk3.0 option for opencv

--- conanfile.py
+++ conanfile.py
@@ -3,20 +3,15 @@ from conans import ConanFile, CMake
 
 class VscodeCppCmakeConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = "opencv/4.1.1@conan/stable", "gtest/1.10.0@conan/stable"
+    requires = "opencv/4.3.0@conan/stable", "gtest/1.10.0"
     generators = "cmake"
 
     def configure(self):
-        # We can control the options of our dependencies based on current options
-        if self.settings.os == "Linux":
-            self.options["opencv"].gtk = 3
 
-        if self.settings.compiler in ["gcc", "clang"] and self.settings.compiler.libcxx == "libstdc++":
+        if (
+            self.settings.compiler in ["gcc", "clang"]
+            and self.settings.compiler.libcxx == "libstdc++"
+        ):
             # conanfile.py can only modify local package options but not global settings
             # thus you cannot modify settings.compiler.libcxx and have to raise an exception
             raise Exception("This package is only compatible with libstdc++11")
-
-    def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
